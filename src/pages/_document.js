ab1d3515b0601662df7ae56ed4b312ca9e9012c4
Marina Tanaka <mtanakasg@gmail.com>
2024-01-17 09:57:05 -0300

    adicionado favicon, mudança no idioma, e metatags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head>
+        <link
+          rel="shortcut icon"
+          href="/images/favicon.png"
+          type="image/png"
+          sizes="256x256"
+        />
+
+        <meta name="author" content="Marina Tanaka" />
+        <meta name="keywords" content="pets, cat, dog, petshop" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
