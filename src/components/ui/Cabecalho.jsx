4e65f346b546c681e59b9520fc55326d8a068cba
Marina Tanaka <mtanakasg@gmail.com>
2024-01-18 09:07:02 -0300

    Correção da image dentro do link e criação do css para o logo e h1

--- src/components/ui/Cabecalho.jsx
+++ src/components/ui/Cabecalho.jsx
@@ -1,21 +1,42 @@
 import Menu from "./Menu";
 import Link from "next/link";
 import Image from "next/image";
+import styled from "styled-components";
+
+// CSS
+const StyledHeader = styled.header`
+  background-color: #f7f7f7;
+  box-shadow: var(--sombra-box);
+
+  h1 a {
+    text-decoration: none;
+    color: var(--cor-primaria);
+    background-color: var(--cor-primaria-fundo);
+    padding: 0.5rem;
+    border-radius: var(--borda-arredondada);
+    font-size: 1.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
 
 export default function Cabecalho() {
   return (
-    <header>
+    <StyledHeader>
       <h1>
-        <Image
-          src="/images/logo.png"
-          width={48}
-          height={48}
-          alt="Logo do Petshop"
-        />
-        <Link href="/">PetShop</Link>
+        <Link href="/">
+          <Image
+            src="/images/logo.png"
+            width={48}
+            height={48}
+            alt="Logo do Petshop"
+          />
+          PetShop
+        </Link>
       </h1>
 
       <Menu />
-    </header>
+    </StyledHeader>
   );
 }
