13302a94bf89a0b58f75654d2e204b0ac0be12fa
Marina Tanaka <mtanakasg@gmail.com>
2024-01-18 08:42:34 -0300

    Adicionado com o componente do next Image o logo do petshop

--- src/components/ui/Cabecalho.jsx
+++ src/components/ui/Cabecalho.jsx
@@ -1,10 +1,17 @@
-import Link from "next/link";
 import Menu from "./Menu";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Cabecalho() {
   return (
     <header>
       <h1>
+        <Image
+          src="/images/logo.png"
+          width={48}
+          height={48}
+          alt="Logo do Petshop"
+        />
         <Link href="/">PetShop</Link>
       </h1>
 
