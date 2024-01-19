0b27ad4622ff533155299577c2d4f610318e9f9b
Marina Tanaka <mtanakasg@gmail.com>
2024-01-19 08:43:34 -0300

    Update produtos.jsx

--- src/pages/produtos.jsx
+++ src/pages/produtos.jsx
@@ -1,6 +1,6 @@
-import Container from "@/components/ui/Container";
 import Head from "next/head";
 import styled from "styled-components";
+import Container from "@/components/ui/Container";
 
 // CSS
 const StyledProdutos = styled.section`
