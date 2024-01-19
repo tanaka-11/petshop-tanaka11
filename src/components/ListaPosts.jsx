75f6871b49bedfbc1763e04984fee53f7e99bca7
Marina Tanaka <mtanakasg@gmail.com>
2024-01-19 11:50:43 -0300

    criado componente para a lista de posts

---
+++ src/components/ListaPosts.jsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import styled from "styled-components";
+
+// CSS
+const StyledListaPosts = styled.div`
+  article {
+    background-color: #f7f7f7;
+    padding: 1rem;
+    margin-bottom: 1rem;
+    box-shadow: var(--sombra-box);
+    border-radius: var(--borda-arredondada);
+    transition: transform 200ms;
+
+    &:hover {
+      cursor: pointer;
+      transform: scale(1.05);
+    }
+
+    & a {
+      text-decoration: none;
+      color: #000;
+
+      &:hover,
+      &:focus {
+        color: #0d0da7;
+      }
+    }
+  }
+
+  @media screen and (min-width: 500px) {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+    article {
+      width: 49%;
+    }
+  }
+`;
+
+export default function ListaPosts({ posts }) {
+  return (
+    <StyledListaPosts>
+      {posts.map((post) => {
+        return (
+          <article key={post.id}>
+            <Link href="">
+              <h3>{post.titulo}</h3>
+              <p>{post.subtitulo}</p>
+            </Link>
+          </article>
+        );
+      })}
+    </StyledListaPosts>
+  );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,48 +1,12 @@
 import Head from "next/head";
 import styled from "styled-components";
-import Link from "next/link";
+import ListaPosts from "@/components/ListaPosts";
 import arrayPosts from "./api/array-posts";
 
 // CSS
 const StyledHome = styled.section`
   h2::before {
     content: "📰 ";
-  }
-`;
-
-const StyledListaPosts = styled.div`
-  article {
-    background-color: #f7f7f7;
-    padding: 1rem;
-    margin-bottom: 1rem;
-    box-shadow: var(--sombra-box);
-    border-radius: var(--borda-arredondada);
-    transition: transform 200ms;
-
-    &:hover {
-      cursor: pointer;
-      transform: scale(1.05);
-    }
-
-    & a {
-      text-decoration: none;
-      color: #000;
-
-      &:hover,
-      &:focus {
-        color: #0d0da7;
-      }
-    }
-  }
-
-  @media screen and (min-width: 500px) {
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-
-    article {
-      width: 49%;
-    }
   }
 `;
 
@@ -65,18 +29,7 @@ export default function Home() {
       <StyledHome>
         <h2>Pet Notícias</h2>
 
-        <StyledListaPosts>
-          {arrayPosts.map((post) => {
-            return (
-              <article key={post.id}>
-                <Link href="">
-                  <h3>{post.titulo}</h3>
-                  <p>{post.subtitulo}</p>
-                </Link>
-              </article>
-            );
-          })}
-        </StyledListaPosts>
+        <ListaPosts posts={arrayPosts} />
       </StyledHome>
     </>
   );
