ea68776493e9e038f0b576da2545499a5fc708dd
Marina Tanaka <mtanakasg@gmail.com>
2024-01-19 08:28:33 -0300

    criação do conteudo da pagina de serviços

--- src/pages/produtos.jsx
+++ src/pages/produtos.jsx
@@ -23,6 +23,33 @@ export default function Produtos() {
 
       <StyledProdutos>
         <h2>Conheça nossos produtos!</h2>
+
+        <article>
+          <h3>Banho</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+            perferendis quasi saepe amet dicta pariatur quos labore mollitia qui
+            dolores.
+          </p>
+        </article>
+
+        <article>
+          <h3>Tosa</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+            perferendis quasi saepe amet dicta pariatur quos labore mollitia qui
+            dolores.
+          </p>
+        </article>
+
+        <article>
+          <h3>Castração</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+            perferendis quasi saepe amet dicta pariatur quos labore mollitia qui
+            dolores.
+          </p>
+        </article>
       </StyledProdutos>
     </>
   );
