f1ee5f76f47b262786a93ecd53922d67a4cd41ac
Marina Tanaka <mtanakasg@gmail.com>
2024-01-22 09:39:39 -0300

    removido importação do array de posts

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import styled from "styled-components";
 import ListaPosts from "@/components/ListaPosts";
-import arrayPosts from "./api/array-posts";
 
 // CSS
 const StyledHome = styled.section`
