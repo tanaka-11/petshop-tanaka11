0b841a4a390b612159f2d0d06823fd15a367bc1e
Marina Tanaka <mtanakasg@gmail.com>
2024-01-23 09:04:10 -0300

    adicionado função de servidor com o getStaticProps com os dados do db,json

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,30 @@
 import Head from "next/head";
 import styled from "styled-components";
 import ListaPosts from "@/components/ListaPosts";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+
+// Funções de Servidor(Back-End) - Obrigatorio ser exportada e async.
+export async function getStaticProps() {
+  try {
+    // Conexão com a API
+    const resposta = await fetch(`http://10.20.46.32:2112/posts`);
+    const dados = await resposta.json();
+
+    // Tratativa de erro na resposta
+    if (!resposta.ok) {
+      throw new Error(`Erro: ${resposta.status} - ${resposta.statusText}`);
+    }
+
+    // Comunicação com o componente atraves de um objeto de props
+    return {
+      props: {
+        posts: dados,
+      },
+    };
+  } catch (error) {
+    console.error("Erro de conexão: " + error.message);
+  }
+}
 
 // CSS
 const StyledHome = styled.section`
@@ -10,26 +33,9 @@ const StyledHome = styled.section`
   }
 `;
 
-export default function Home() {
+export default function Home({ posts }) {
   // Hooks
-  const [listaDePosts, setlistaDePosts] = useState([]);
-
-  useEffect(() => {
-    const carregarDados = async () => {
-      try {
-        const resposta = await fetch(`http://10.20.46.32:2112/posts`);
-
-        if (!resposta.ok) {
-          throw new Error(`Erro: ${resposta.status} - ${resposta.statusText}`);
-        }
-        const dados = await resposta.json();
-        setlistaDePosts(dados);
-      } catch (error) {
-        console.error("Erro: " + error);
-      }
-    };
-    carregarDados();
-  }, []);
+  const [listaDePosts, setlistaDePosts] = useState(posts);
 
   return (
     <>
