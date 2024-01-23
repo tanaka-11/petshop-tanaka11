07efefb656c0049d6e25af7c483ba0d0b1215b78
Marina Tanaka <mtanakasg@gmail.com>
2024-01-23 11:05:03 -0300

    estrutura da pagina do parametro da rota dinamica

--- src/pages/posts/[id].jsx
+++ src/pages/posts/[id].jsx
@@ -1,3 +1,26 @@
+import Container from "@/components/ui/Container";
+import Head from "next/head";
+import styled from "styled-components";
+
+// CSS
+const StyledPost = styled.article``;
+
 export default function Post() {
-  return <h2>Página Post</h2>;
+  return (
+    <>
+      <Head>
+        <title>Titulo - PetShop</title>
+        <meta name="description" content="Descrição" />
+      </Head>
+
+      <StyledPost>
+        <h2>Titulo...</h2>
+
+        <Container>
+          <h3>Categoria...</h3>
+          <p>Descrição...</p>
+        </Container>
+      </StyledPost>
+    </>
+  );
 }
