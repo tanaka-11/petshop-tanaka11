587e80b5cca6271c31742b361d9eaf3c37ef13f0
Marina Tanaka <mtanakasg@gmail.com>
2024-01-23 09:49:53 -0300

    Create server.js

---
+++ src/pages/api/server.js
@@ -0,0 +1,6 @@
+// configuração de conexão api
+const porta = 2112;
+const endereco = "http://10.20.46.32"; // Em casa mudar para o ip da maquina
+const serverApi = `http://${endereco}:${porta}`;
+
+export default serverApi;
