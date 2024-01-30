702e583c96134580d5cdfd3a1d316455b4cd8d79
Marina Tanaka <mtanakasg@gmail.com>
2024-01-30 09:35:21 -0300

    mudança da api fake para a api via firebase

--- src/pages/api/server.js
+++ src/pages/api/server.js
@@ -1,6 +1,9 @@
-// Porta e Endereço IP
-const porta = 2112;
-const endereco = "10.20.46.32"; // Em casa mudar para o ip da maquina
-const serverApi = `http://${endereco}:${porta}`;
+// Porta e Endereço IP para uso da API Fake
+// const porta = 2112;
+// const endereco = "10.20.46.32"; - Em casa mudar para o ip da maquina
+// const serverApi = `http://${endereco}:${porta}`;
+
+// API via Firebase
+const serverApi = `https://petshop-tanaka11-default-rtdb.firebaseio.com/`;
 
 export default serverApi;
