bd100df235ed08ca8741268ed201444a7edb1e90
Marina Tanaka <mtanakasg@gmail.com>
2024-01-31 10:47:06 -0300

    Update Cabecalho.jsx

--- src/components/ui/Cabecalho.jsx
+++ src/components/ui/Cabecalho.jsx
@@ -5,7 +5,7 @@ import styled from "styled-components";
 
 // CSS
 const StyledHeader = styled.header`
-  background-color: #daf5ff;
+  background-color: #c9ecf8;
   box-shadow: var(--sombra-box);
   border-radius: 10px;
 
